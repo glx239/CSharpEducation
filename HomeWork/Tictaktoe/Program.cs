a025384f37eab6a2075760ec58647690a5bb4045
glx239 <163163756+glx239@users.noreply.github.com>
2025-05-03 14:40:34 +0400

    Update Program.cs

--- HomeWork/Tictaktoe/Program.cs
+++ HomeWork/Tictaktoe/Program.cs
@@ -1,96 +1,12 @@
-﻿namespace tictaktoe;
+namespace tictaktoe;
 
 class Program
 {
     static void Main(string[] args)
     {
-        void  Visual(int[] array){
-            for (int i = 0; i < array.Length; i++){
-                if ((i + 1) % 3 == 0){
-                    if (array[i] == -1)
-                    {
-                        Console.WriteLine('X');
-                    }
-                    else if (array[i] == 0)
-                    {
-                        Console.WriteLine(0);
-                    }
-                    else
-                    {
-                        Console.WriteLine(array[i]);
-                    }
-
-                }
-                else
-                {
-                    if (array[i] == -1)
-                    {
-                        Console.Write('X' + "|");
-                    }
-                    else if (array[i] == 0)
-                    {
-                        Console.Write(0 + "|");
-                    }
-                    else
-                    {
-                        Console.Write(array[i] + "|");
-                        
-                    }
-                }
-                    
-            }
-        }
-        
-        
-
-        bool Check(int[] array, int num, bool turn){
-            
-            if (array[num - 1] == 0 ||  array[num - 1] == -1){
-                Console.WriteLine("Данная клетка занята, введите другую");
-                return turn;
-            }
-            if (turn){
-                array[num - 1] = -1;
-                
-                return false;
-            }
-            else{
-                array[num - 1] = 0;
-                return true;
-            }
-        }
-
-        bool CheckDraw(int[] array)
-        { 
-            int count = 0;
-            foreach (int i in array)
-            {
-
-                if (i == -1  || i == 0)
-                {
-                    count++;
-                }
-            }
-            return count == 9 ? true : false;
-        }
-
-        static bool CheckWin(int[] array)
-        {
-            return (array[0] == array[1] && array[1] == array[2]) ||
-                   (array[3] == array[4] && array[4] == array[5]) ||
-                   (array[6] == array[7] && array[7] == array[8]) ||
-                   (array[0] == array[3] && array[3] == array[6]) ||
-                   (array[1] == array[4] && array[4] == array[7]) ||
-                   (array[2] == array[5] && array[5] == array[8]) ||
-                   (array[0] == array[4] && array[4] == array[8]) ||
-                   (array[2] == array[4] && array[4] == array[6]);
-        }
         
         bool turn = true; // 0 - крестики, 1 - нолики.
         int [] board = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-
-
-
 
         while (true){
             Visual(board);
@@ -132,5 +48,84 @@ class Program
             }
 
         }
+    }
+    
+    static void  Visual(int[] array){
+        for (int i = 0; i < array.Length; i++){
+            if ((i + 1) % 3 == 0){
+                if (array[i] == -1)
+                {
+                    Console.WriteLine('X');
+                }
+                else if (array[i] == 0)
+                {
+                    Console.WriteLine(0);
+                }
+                else
+                {
+                    Console.WriteLine(array[i]);
+                }
+            }
+            else
+            {
+                if (array[i] == -1)
+                {
+                    Console.Write('X' + "|");
+                }
+                else if (array[i] == 0)
+                {
+                    Console.Write(0 + "|");
+                }
+                else
+                {
+                    Console.Write(array[i] + "|");
+                        
+                }
+            }
+                    
+        }
+    }
+    
+    static bool Check(int[] array, int num, bool turn){
+            
+        if (array[num - 1] == 0 ||  array[num - 1] == -1){
+            Console.WriteLine("Данная клетка занята, введите другую");
+            return turn;
+        }
+        if (turn){
+            array[num - 1] = -1;
+                
+            return false;
+        }
+        else{
+            array[num - 1] = 0;
+            return true;
+        }
+    }
+
+    static bool CheckDraw(int[] array)
+    { 
+        int count = 0;
+        foreach (int i in array)
+        {
+
+            if (i == -1  || i == 0)
+            {
+                count++;
+            }
+        }
+        return count == 9 ? true : false;
+    }
+
+    static bool CheckWin(int[] array)
+    {
+        return (array[0] == array[1] && array[1] == array[2]) ||
+               (array[3] == array[4] && array[4] == array[5]) ||
+               (array[6] == array[7] && array[7] == array[8]) ||
+               (array[0] == array[3] && array[3] == array[6]) ||
+               (array[1] == array[4] && array[4] == array[7]) ||
+               (array[2] == array[5] && array[5] == array[8]) ||
+               (array[0] == array[4] && array[4] == array[8]) ||
+               (array[2] == array[4] && array[4] == array[6]);
     }
 }
