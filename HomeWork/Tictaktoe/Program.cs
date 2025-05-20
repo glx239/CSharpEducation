11a3bdf04a2ec269bd731b615b21082733a6bb7b
glx239 <toxa53101@yandex.ru>
2025-05-20 19:31:44 +0400

    tictaktoe
    refactoring

--- HomeWork/Tictaktoe/Program.cs
+++ HomeWork/Tictaktoe/Program.cs
@@ -1,21 +1,23 @@
+﻿using System;
+
 namespace tictaktoe;
 
 class Program
 {
     static void Main(string[] args)
     {
+        TicTakToe tictaktoe = new TicTakToe();
         
-        bool turn = true; // 0 - крестики, 1 - нолики.
-        int [] board = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        Start(tictaktoe);
+    }
 
-        while (true){
-            Visual(board);
-            if (CheckDraw(board) == true)
-            {
-                Console.Write("Ничья");
-                break;
-            }            
-            if (turn){
+    static void Start(TicTakToe tictaktoe)
+    {
+        while (tictaktoe.CheckTie(tictaktoe.board))
+        {
+            tictaktoe.DrawBoard(tictaktoe.board);
+            
+            if (tictaktoe.CurrentPlayer){
                 Console.Write("Ходит первый игрок\nВведите цифру: ");
             }
             else{
@@ -26,7 +28,7 @@ class Program
             try
             {
                 int num = Convert.ToInt32(Console.ReadLine());
-                turn = Check(board, num, turn);
+                tictaktoe.CurrentPlayer = tictaktoe.Check(tictaktoe.board, num, tictaktoe.CurrentPlayer);
             }
             catch (Exception)
             {
@@ -34,9 +36,9 @@ class Program
                 continue;
             }
 
-            if (CheckWin(board))
+            if (tictaktoe.CheckWin(tictaktoe.board))
             {
-                if (turn)
+                if (tictaktoe.CurrentPlayer)
                 {
                     Console.WriteLine("Победил второй игрок");
                 }
@@ -49,83 +51,6 @@ class Program
 
         }
     }
-    
-    static void  Visual(int[] array){
-        for (int i = 0; i < array.Length; i++){
-            if ((i + 1) % 3 == 0){
-                if (array[i] == -1)
-                {
-                    Console.WriteLine('X');
-                }
-                else if (array[i] == 0)
-                {
-                    Console.WriteLine(0);
-                }
-                else
-                {
-                    Console.WriteLine(array[i]);
-                }
-            }
-            else
-            {
-                if (array[i] == -1)
-                {
-                    Console.Write('X' + "|");
-                }
-                else if (array[i] == 0)
-                {
-                    Console.Write(0 + "|");
-                }
-                else
-                {
-                    Console.Write(array[i] + "|");
-                        
-                }
-            }
-                    
-        }
-    }
-    
-    static bool Check(int[] array, int num, bool turn){
-            
-        if (array[num - 1] == 0 ||  array[num - 1] == -1){
-            Console.WriteLine("Данная клетка занята, введите другую");
-            return turn;
-        }
-        if (turn){
-            array[num - 1] = -1;
-                
-            return false;
-        }
-        else{
-            array[num - 1] = 0;
-            return true;
-        }
-    }
 
-    static bool CheckDraw(int[] array)
-    { 
-        int count = 0;
-        foreach (int i in array)
-        {
-
-            if (i == -1  || i == 0)
-            {
-                count++;
-            }
-        }
-        return count == 9 ? true : false;
-    }
-
-    static bool CheckWin(int[] array)
-    {
-        return (array[0] == array[1] && array[1] == array[2]) ||
-               (array[3] == array[4] && array[4] == array[5]) ||
-               (array[6] == array[7] && array[7] == array[8]) ||
-               (array[0] == array[3] && array[3] == array[6]) ||
-               (array[1] == array[4] && array[4] == array[7]) ||
-               (array[2] == array[5] && array[5] == array[8]) ||
-               (array[0] == array[4] && array[4] == array[8]) ||
-               (array[2] == array[4] && array[4] == array[6]);
-    }
 }
+    
